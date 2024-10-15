d8fbc5e598fab978b0d51a25ab8323df48b6aa2f
dragonahao <anhao.zheng@lucahealthcare.com>
2024-10-15 16:00:58 +0800

    feat 1.修改ZZPackage

--- Sources/LibrarysubB/LibraryB.swift
+++ Sources/LibrarysubB/LibraryB.swift
@@ -1,10 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import Foundation
-
+import Swift
 @objc
 public class LibraryB: NSObject {
     @objc open func printHello() {
         print("Hello from LibraryB")
+
     }
 }
--- Sources/ZZPackage/ZZPackage.swift
+++ Sources/ZZPackage/ZZPackage.swift
@@ -1,8 +1,29 @@
 import Foundation
+import SwiftLibraryFirstSubA
 
 @objcMembers
 open class ZZPackage: NSObject {
     var computedName: String {
         return "ZZPackage-computed-name"
+    }
+
+    public func publicMethod() {
+        LibraryA().printHello()  // vscode对子包的make sense 不友好，能用但是没提示
+        // LibraryB() //没有导入SwiftLibraryFirstSubB无法使用
+    }
+
+    open func openMethod() {
+        print("ZZPackage open method")
+        self.publicMethod()
+        self.privateMethod()
+        self.internalMethod()
+    }
+
+    private func privateMethod() {
+        print("ZZPackage private method")
+    }
+
+    internal func internalMethod() {
+        print("ZZPackage internal method")
     }
 }
